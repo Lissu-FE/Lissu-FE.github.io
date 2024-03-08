892bfbe2bad838edc2c13ad580925684f2c4eeb3
Lissu-FE <mary567@hanmail.net>
2024-03-08 11:30:41 +0900

    update work experience

--- components/aboutLissu/WorkExperience/WorkExperience.tsx
+++ components/aboutLissu/WorkExperience/WorkExperience.tsx
@@ -10,19 +10,25 @@ const WorkExperience = () => {
     <WorkExperienceWrapper>
       <MainTitle>Work Experience 💻</MainTitle>
       <WorkBoxSection>
-        <MyPosition>Middle-Tier Web Developer</MyPosition>
-        <div>비즈아이솔루션</div>
-        <div>2017.02 ~ 2019.08</div>
+        <MyPosition>Lead Front-End Developer</MyPosition>
+        <div>B factory</div>
+        <div>2023.04 ~ </div>
+      </WorkBoxSection>
+      <WorkBoxSection>
+        <MyPosition>Lead Front-End Developer</MyPosition>
+        <div>Blinkers</div>
+        <div>2022.09 ~ 2023.02</div>
       </WorkBoxSection>
       <WorkBoxSection>
         <MyPosition>Front-End Developer</MyPosition>
         <div>next level studio</div>
         <div>2019.11 ~ 2022.08</div>
       </WorkBoxSection>
+
       <WorkBoxSection>
-        <MyPosition>Lead Front-End Developer</MyPosition>
-        <div>Blinkers</div>
-        <div>2022.09 ~ </div>
+        <MyPosition>Middle-Tier Web Developer</MyPosition>
+        <div>비즈아이솔루션</div>
+        <div>2017.02 ~ 2019.08</div>
       </WorkBoxSection>
     </WorkExperienceWrapper>
   )
